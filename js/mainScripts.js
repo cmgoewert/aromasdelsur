494d37e3f1ce3a988c04f4cbf228154e5dc0947d
Chandler Goewert <cmgoewert@aol.com>
2018-04-16 23:29:37 -0400

    added banner, contact working

--- js/mainScripts.js
+++ js/mainScripts.js
@@ -1,4 +1,62 @@
 var messages;
+var slideIndex = Math.floor(Math.random() * 29);
+
+function populateSlideshow() {
+    var innerHTMLstring = ""
+    for(var i = 0; i < 30; i++){
+        innerHTMLstring += "<img class='mySlides w3-animate-bottom' src='images/slideshow/"+(i+1) +".jpg' />"
+    }
+
+    innerHTMLstring += "<div class='w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle' style='width:100%'><div class='w3-left w3-hover-text-khaki' onclick='plusDivs(-1)'>&#10094;</div><div class='w3-right w3-hover-text-khaki' onclick='plusDivs(1)'>&#10095;</div>";
+    for(var i = 0; i < 30; i++){
+        innerHTMLstring+= "<span class='w3-badge demo w3-border w3-transparent w3-hover-white' onclick='currentDiv("+(i+1)+")'></span>";
+    }
+
+
+
+    document.getElementById("slideshow").innerHTML = innerHTMLstring;
+    
+    showDivs(slideIndex);
+    carousel();
+}
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+  
+  function currentDiv(n) {
+    showDivs(slideIndex = n);
+  }
+  
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+       dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " w3-white";
+  }
+
+
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
 
 $('#contactForm').submit(function () {
     sendEmail();
